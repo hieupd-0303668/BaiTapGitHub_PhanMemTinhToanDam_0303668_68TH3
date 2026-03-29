0950f88b5f3ed59be7397045e1502e5e629a4135
hieupd_0303668 <0303668@st.huce.edu.vn>
2026-03-29 23:56:46 +0700

    code model

---
+++ Models/RebarMaterialModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GK_LTUDTXD_PhamDucHieu_0303668_68TH3.Models
+{
+    public class RebarMaterialModel
+    {
+        public string Name { get; set; }
+
+        public double Rs { get; set; }
+        public double Rsc { get; set; }
+    }
+}
